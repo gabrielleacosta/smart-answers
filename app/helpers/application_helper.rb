f694f29f11e2b1b141e768a7b16e8368a228f721
Anika Henke <anika.henke@digital.cabinet-office.gov.uk>
2016-08-02 19:45:22 +0100

    Disable AJAX for some Smart Answers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,10 @@ module ApplicationHelper
   def last_updated_date
     File.mtime(Rails.root.join('REVISION')).to_date rescue Date.today
   end
+
+  def ajax_enabled_for?(name)
+    %w(
+      all-smart-answer-questions
+    ).exclude?(name)
+  end
 end
